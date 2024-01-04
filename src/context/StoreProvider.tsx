19dd8697e169d15e72ddbdb20f7e8dd14ccfaf5f
Rudi Aj <raj@dickeys.com>
2024-01-04 12:22:59 +0100

    fix: remove locations

--- src/context/StoreProvider.tsx
+++ src/context/StoreProvider.tsx
@@ -1,13 +1,10 @@
 import { createContext, useMemo, useContext, useState } from 'react';
 
 import type { FC, ReactNode } from 'react';
-import type { Location } from '@components/pages/SmoothieFactory/OrangeLeaf/Locations/SearchResults/SearchResults';
 import { initialCoordinates } from '@constants/initialCoordinates';
 
 type ContextValue = {
   setIsGoogleScriptLoaded: (value: boolean) => void;
-  setLocations: (value: Location[]) => void;
-  locations: Location[];
   isGoogleScriptLoaded: boolean;
   searchResultCoordinates: { lat: number; lng: number };
   setSearchResultCoordinates: (value: { lat: number; lng: number }) => void;
@@ -18,16 +15,13 @@ type ContextValue = {
 const StoreContext = createContext<ContextValue>({
   footerHeight: 0,
   isGoogleScriptLoaded: false,
-  locations: [],
   searchResultCoordinates: { lat: 0, lng: 0 },
   setFooterHeight: () => undefined,
   setIsGoogleScriptLoaded: () => undefined,
-  setLocations: () => undefined,
   setSearchResultCoordinates: () => undefined,
 });
 
 export const StoreProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const [locations, setLocations] = useState<Location[]>([]);
   const [isGoogleScriptLoaded, setIsGoogleScriptLoaded] = useState(false);
   const [searchResultCoordinates, setSearchResultCoordinates] = useState(initialCoordinates);
   const [footerHeight, setFooterHeight] = useState(0);
@@ -36,16 +30,12 @@ export const StoreProvider: FC<{ children: ReactNode }> = ({ children }) => {
     () => ({
       footerHeight,
       isGoogleScriptLoaded,
-      locations,
       searchResultCoordinates,
       setFooterHeight,
       setIsGoogleScriptLoaded,
-      setLocations,
       setSearchResultCoordinates,
     }),
     [
-      locations,
-      setLocations,
       isGoogleScriptLoaded,
       setIsGoogleScriptLoaded,
       searchResultCoordinates,
