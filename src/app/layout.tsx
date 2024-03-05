6be69e324b7a673799fe10203c0a0188af178909
Rudi Aj <raj@dickeys.com>
2024-03-05 18:29:47 +0100

    feat: add gtag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import StyledComponentsRegistry from '@styles/registry';
 import type { FC, ReactNode } from 'react';
 import GlobalStyle from './GlobalStyle';
 import { StoreProvider } from 'src/context/StoreProvider';
+import Script from 'next/script';
 
 const variableBlack = localFont({
   src: [
@@ -28,7 +29,24 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
         <link rel="stylesheet" href="https://use.typekit.net/cst1rrm.css" />
       </head>
       <html className={`${variableBlack.variable}`} lang="en">
+        <Script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NSZ2RDB');
+
+`}
+        </Script>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NSZ2RDB"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <StoreProvider>
             <StyledComponentsRegistry>
               <GlobalStyle />
